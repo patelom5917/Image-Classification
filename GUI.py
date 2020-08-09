ad9cad8458a35ff786048c7564b73fbe9915e56b
om patel <58931580+patelom5917@users.noreply.github.com>
2020-08-10 00:51:34 +0530

    Create GUI.py

---
+++ GUI.py
@@ -0,0 +1,73 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+import numpy
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+
+#load model
+model = load_model('cats_and_dogs_small_2.h5')
+
+#dictionary to label all traffic signs class.
+classes = { 
+    0:'its a cat',
+    1:'its a dog',
+}
+
+#initialise GUI
+top=tk.Tk()
+top.geometry('800x600')
+top.title('CatsVSDogs Classification')
+top.configure(background='#CDCDCD')
+label=Label(top,background='#CDCDCD', font=('arial',15,'bold'))
+sign_image = Label(top)
+def classify(file_path):
+    global label_packed
+    image = Image.open(file_path)
+    image = image.resize((150,150))
+    image = img_to_array(image)
+    image = numpy.expand_dims(image, axis=0)
+    
+    image = image/255
+    pred = model.predict_classes([image])[0]
+    print(pred)
+    #sign = classes[pred]
+    if(pred== [0]):
+        sign='its a cat'
+    else:
+        sign='its a dog'
+    
+    label.configure(foreground='#011638', text=sign) 
+    
+def show_classify_button(file_path):
+    classify_b=Button(top,text="Classify Image",
+   command=lambda: classify(file_path),
+   padx=10,pady=5)
+    classify_b.configure(background='#364156', foreground='white',
+font=('arial',10,'bold'))
+    classify_b.place(relx=0.79,rely=0.46)
+    
+def upload_image():
+    try:
+        file_path=filedialog.askopenfilename()
+        uploaded=Image.open(file_path)
+        uploaded.thumbnail(((top.winfo_width()/2.25),
+    (top.winfo_height()/2.25)))
+        im=ImageTk.PhotoImage(uploaded)
+        sign_image.configure(image=im)
+        sign_image.image=im
+        label.configure(text='')
+        show_classify_button(file_path)
+    except:
+        pass
+
+upload=Button(top,text="Upload an image",command=upload_image,padx=10,pady=5)
+upload.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
+upload.pack(side=BOTTOM,pady=50)
+sign_image.pack(side=BOTTOM,expand=True)
+label.pack(side=BOTTOM,expand=True)
+heading = Label(top, text="CatsVSDogs Classification",pady=20, font=('arial',20,'bold'))
+heading.configure(background='#CDCDCD',foreground='#364156')
+heading.pack()
+top.mainloop()
